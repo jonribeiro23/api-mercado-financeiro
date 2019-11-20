63d9dd3518f4bab835ba5b3bcf7b26993844cbcc
jonribeiro23 <jonathas_tc@hotmail.com>
2019-11-19 21:41:10 -0300

    banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+
+# Cria o banco de dados
+# app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+# Inicializa o banco
+db = SQLAlchemy(app)
+
+
+# classe que modela o banco
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id # retorna o id da tarefa
+
 
 @app.route('/')
 def index():
