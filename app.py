4fe216148c9bd1ba40c8005bde564b5e0ceca465
jonribeiro23 <jonathas_tc@hotmail.com>
2019-11-19 22:14:36 -0300

    deletar tarefa

--- app.py
+++ app.py
@@ -39,5 +39,21 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Erro ao deletar tarefa'
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
